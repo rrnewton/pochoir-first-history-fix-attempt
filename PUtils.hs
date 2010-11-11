1539b361b59de20d7df23bebcaf240802e7d0ecd
yuantang <yuantang@4b2bd529-2931-0410-9ed4-fa3a767a38ed>
2010-11-11 01:13:35 +0000

    add in support of Pochoir variables as input parameters
    git-svn-id: https://svn.csail.mit.edu/sct/trunk/Pochoir/ExecSpec_refine@18736 4b2bd529-2931-0410-9ed4-fa3a767a38ed

---
+++ PUtils.hs
@@ -0,0 +1,130 @@
+{-
+ ----------------------------------------------------------------------------------
+ -  Copyright (C) 2010  Massachusetts Institute of Technology
+ -  Copyright (C) 2010  Yuan Tang <yuantang@csail.mit.edu>
+ -                      Charles E. Leiserson <cel@mit.edu>
+ -   
+ -   This program is free software: you can redistribute it and/or modify
+ -   it under the terms of the GNU General Public License as published by
+ -   the Free Software Foundation, either version 3 of the License, or
+ -   (at your option) any later version.
+ -
+ -   This program is distributed in the hope that it will be useful,
+ -   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ -   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ -   GNU General Public License for more details.
+ -
+ -   You should have received a copy of the GNU General Public License
+ -   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ -
+ -   Suggestsions:                  yuantang@csail.mit.edu
+ -   Bugs:                          yuantang@csail.mit.edu
+ -
+ --------------------------------------------------------------------------------
+ -}
+
+module PUtils where
+
+import Text.ParserCombinators.Parsec
+import Control.Monad
+import Data.List
+import PData
+import qualified Data.Map as Map
+
+updatePKernel :: PKernel -> ParserState -> ParserState
+updatePKernel l_kernel parserState =
+    parserState { pKernel = Map.insert (kName l_kernel) l_kernel $ pKernel parserState }
+
+updateObase :: PMode -> ParserState -> ParserState
+updateObase mode parserState = parserState { pMode = mode }
+
+updatePState :: PState -> ParserState -> ParserState
+updatePState pstate parserState
+    | (pstate == PochoirBegin && pState parserState == Unrelated) = parserState { pState = PochoirBegin }
+    | (pstate == PochoirEnd && pState parserState == PochoirBegin) = parserState { pState = Unrelated }
+    | ((pstate == PochoirMacro || pstate == PochoirDeclArray || pstate == PochoirDeclRange)
+            && pState parserState == Unrelated) = parserState { pState = pstate }
+    | pstate == Unrelated = parserState { pState = Unrelated }
+    | otherwise = parserState { pState = PochoirError }
+
+updatePMacro :: (PName, PValue) -> ParserState -> ParserState
+updatePMacro (l_name, l_value) parserState =
+    parserState { pMacro = Map.insert l_name l_value (pMacro parserState) }
+
+updatePShape :: (PName, Int, PValue, [[Int]]) -> ParserState -> ParserState
+updatePShape (l_name, l_rank, l_len, l_shape) parserState =
+    let l_pShape = PShape {shapeName = l_name, shapeRank = l_rank, shapeLen = l_len, shape = l_shape}
+    in parserState { pShape = Map.insert l_name l_pShape (pShape parserState) }
+
+updatePArray :: [(PName, PArray)] -> ParserState -> ParserState
+updatePArray [] parserState = parserState
+updatePArray pL@(p:ps) parserState =
+    parserState { pArray = foldr pMapInsert (pArray parserState) pL }
+
+updatePStencil :: [(PName, PStencil)] -> ParserState -> ParserState
+updatePStencil [] parserState = parserState
+updatePStencil pL@(p:ps) parserState =
+    parserState { pStencil = foldr pMapInsert (pStencil parserState) pL }
+
+updatePRange :: [(PName, PRange)] -> ParserState -> ParserState
+updatePRange [] parserState = parserState
+updatePRange pL@(p:ps) parserState =
+    parserState { pRange = foldr pMapInsert (pRange parserState) pL }
+
+pMapInsert :: (Ord k) => (k, a) -> Map.Map k a -> Map.Map k a
+pMapInsert (l_key, l_value) l_map = Map.insert l_key l_value l_map
+
+updateStencilArray :: String -> PArray -> ParserState -> ParserState
+updateStencilArray l_id l_pArray parserState =
+    let f k x =
+            if sName x == l_id
+                then Just $ x { sArrayInUse = union [l_pArray] (sArrayInUse x) }
+                else Nothing
+    in  parserState { pStencil = Map.updateWithKey f l_id $ pStencil parserState }
+
+updateStencilBoundary :: String -> Bool -> ParserState -> ParserState
+updateStencilBoundary l_id l_regBound parserState =
+    let f k x =
+            if sName x == l_id
+                then Just $ x { sRegBound = l_regBound }
+                else Nothing
+    in  parserState { pStencil = Map.updateWithKey f l_id $ pStencil parserState }
+
+getIter :: PArray -> Expr -> [Iter]
+getIter arrayInUse (PVAR v dL) =
+    let iterName = "iter"
+    in  [(iterName, arrayInUse, dL)]
+getIter arrayInUse _ = []
+
+getPointer :: [PName] -> PArray -> Expr -> [Iter]
+getPointer kernelParams arrayInUse (PVAR v dL) =
+    let iterName = "pt_" ++ aName arrayInUse ++ "_"
+        dL' = transDimExpr kernelParams dL
+    in  [(iterName, arrayInUse, dL')]
+getPointer _ arrayInUse _ = []
+
+transIterN :: Int -> [Iter] -> [Iter]
+transIterN _ [] = []
+transIterN n ((name, array, dim):is) = (name ++ show n, array, dim) : (transIterN (n+1) is)
+
+transArrayMap :: [PArray] -> Map.Map PName PArray
+transArrayMap aL = Map.fromList $ transAssocList aL
+    where transAssocList [] = []
+          transAssocList aL@(a:as) = [(aName a, a)] ++ (transAssocList as)
+
+transDimExpr :: [PName] -> [DimExpr] -> [DimExpr]
+transDimExpr kL [] = []
+transDimExpr kL dL@(d:ds) = [d] ++ (transSpaceDimExpr ds)
+    where transSpaceDimExpr [] = []
+          transSpaceDimExpr (d:ds) = (transSpaceDimExprItem d) ++ (transSpaceDimExpr ds)
+          transSpaceDimExprItem (DimVAR v) =
+                        if elem v kL then [(DimVAR v)]
+                                     else []
+          transSpaceDimExprItem (DimDuo bop e1 e2) =
+                        transSpaceDimExprItem e1 ++ transSpaceDimExprItem e2
+          transSpaceDimExprItem (DimINT n) = []
+
+pFirst (a, _, _) = a
+pSecond (_, b, _) = b
+pThird (_, _, c) = c
+
